bad2224c1c4efab696647e6d4d63f67b7d2d8424
GABRIEL CUNHA CAMPOS DIEGUEZ <101563389+gcunha-campos@users.noreply.github.com>
2023-09-20 12:14:41 -0300

    Add files via upload

---
+++ carta.java
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CartaoDeVisitas extends React.Component {
+  render() {
+    return (
+      <div className="cartao-de-visitas">
+        <h1>Meu Business Card</h1>
+        <p>Nome: Gabriel Cunha Campos</p>
+        <p>E-mail: gabrielcunha06@gmail.com</p>
+        <p>Telefone: (61) 98123-5823</p>
+      </div>
+    );
+  }
+}
+
+export default CartaoDeVisitas;
+
